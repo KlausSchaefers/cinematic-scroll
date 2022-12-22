f5e918d10b4ac5b8d57c3b61451a6c4da6c49123
KlausSchaefers <klaus.schaefers@gmail.com>
2022-12-22 23:25:45 +0000

    Done

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
 import './style.css'
 import cinematic from './src/cinematic'
 
-cinematic('target', 'Lili is the best in the world')
+cinematic('target', 'Lili is the best in the world! Thanks for our children. You make things!')
--- src/cinematic.js
+++ src/cinematic.js
@@ -1,15 +1,102 @@
 import './cinematic.css'
 
+const chars = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
+
+let renderId = null
+
 function create(parent,  className, type = 'div') {
     const node = document.createElement(type)
     node.className = className
-    parent.append(node)
+    if (parent){
+        parent.appendChild(node)
+    }
     return node
 }
 
-function createText(node, text) {
+function getRowsAndCols(parent) {
+ 
+    const node = document.createElement('span')
+    node.innerHTML = 'A'
+    parent.appendChild(node)
+    
+    const parentW = parent.offsetWidth
+    const parentH = parent.offsetHeight
+    const charW = node.offsetWidth
+    const charH = node.offsetHeight
+    const rows = Math.floor(parentH / charH)
+    const cols = Math.floor(parentW / charW)
 
-    return []
+    parent.innerHTML = ''
+    return [rows, cols]
+}
+
+function getTokens(text, rows) {
+    let tokens = text.split(' ')
+    if (tokens.length > rows - 2) {
+    
+        let newTokens = []
+        let tokensPerRow = Math.ceil(tokens.length / (rows - 2))
+        console.debug('Merge', tokensPerRow, rows)
+
+        let merged = []
+        tokens.forEach((token,i) => {
+            merged.push(token)
+            if (merged.length === tokensPerRow) {
+                newTokens.push(merged.join('').toUpperCase())
+                merged = []
+            }
+        })
+        tokens = newTokens
+    }
+    while (tokens.length < rows - 2) {
+        let pos = Math.round(tokens.length * Math.random())
+        tokens.splice(pos,0,'')
+    }
+    return tokens
+}
+
+function createText(parent, text, spaceChars = 10) {
+    const randomNodes = []
+    const [rows, cols] = getRowsAndCols(parent)
+
+    const tokens = getTokens(text, rows)
+    console.debug('tokens', tokens)
+
+
+   
+    const content = document.createElement('div')
+    const width = (cols - spaceChars)
+    for (let r = 0; r < rows; r++) {
+        let row = document.createElement('div')
+        row.className = 'cinematic-content-row'
+        let tokenOffset = 10000
+        let token = ''
+        if (r > 0 && tokens.length > 0) {
+            token = tokens.shift()
+            tokenOffset = (cols - spaceChars) - token.length
+            tokenOffset = Math.max(1, Math.round(tokenOffset * Math.random()))   
+        }
+
+        for (let c = 0; c < width; c++) {
+            const node = document.createElement('span')
+            node.style.color = `rgba(0,0,0,${0.8 + 0.2 * Math.random()})`
+
+            if (token !== '' && c >= tokenOffset && (tokenOffset + token.length) > c ) {
+                const char = token[c - tokenOffset]
+                node.innerHTML = char
+                node.className = 'cinematic-content-correct'
+            } else {
+                const char = Math.floor(Math.random() * chars.length)
+                node.innerHTML = chars[char]
+                randomNodes.push(node)
+            }
+            row.appendChild(node)
+        }
+        content.appendChild(row)
+    }
+    parent.appendChild(content)
+
+    return randomNodes
 }
 
 export default function cinematic(parentId, text, speed = 10) {
@@ -24,11 +111,27 @@ export default function cinematic(parentId, text, speed = 10) {
     scroll.style.height = (speed * 100) + '%'
     const content = create(cntr, 'cinematic-content')
 
-    const fillNodes = createText(content, text)
+    const randomNodes = createText(content, text)
     const h = scroll.offsetHeight
 
     cntr.addEventListener("scroll", (event) => {
-
-        console.debug('scroll', cntr.scrollTop / h )
+        const p = cntr.scrollTop / h
+        renderP(p, randomNodes)
     });
+}
+
+function renderP(p, randomNodes) {
+    if (renderId) {
+        cancelAnimationFrame(renderId)
+    }
+    renderId = requestAnimationFrame(() => {
+        let end = Math.ceil(p * randomNodes.length)
+        randomNodes.forEach((node, i) => {
+            if (i < end) {
+                node.className = 'cinematic-content-strike'
+            } else {
+                node.className = ''
+            }
+        })
+    })
 }
